ebeca1230b54925cbab1695327b65d3973fab661
Jack Danger Canty <gitcommit@6brand.com>
2011-01-03 11:36:49 -0800

    fixing expectation

--- lib/geoip_server.rb
+++ lib/geoip_server.rb
@@ -30,7 +30,7 @@ get '/:ip' do
 
   data = GeoIP.new(data_file).city(params[:ip])
 
-  content_type 'application/json'
+  content_type 'application/json;charset=utf-8'
   headers['Cache-Control'] = "public; max-age=#{365*24*60*60}"
 
   return "{}" unless data
